bfe06666e32d9c663dca7fd1fe0484642840ece0
chenxue03 <chenxue03@baidu.com>
2024-12-30 15:55:24 +0800

    Update AdminUserDetails.java

--- mall-admin/src/main/java/com/macro/mall/bo/AdminUserDetails.java
+++ mall-admin/src/main/java/com/macro/mall/bo/AdminUserDetails.java
@@ -62,4 +62,17 @@ public class AdminUserDetails implements UserDetails {
     public boolean isEnabled() {
         return umsAdmin.getStatus().equals(1);
     }
+    
+    // 这是一个冒泡排序
+	public void bubbleSort(List<UmsResource> list) {
+		for (int i = 0; i < list.size() - 1; i++) {
+			for (int j = 0; j < list.size() - 1 - i; j++) {
+				if (list.get(j).getSort() > list.get(j + 1).getSort()) {
+					UmsResource temp = list.get(j);
+					list.set(j, list.get(j + 1));
+					list.set(j + 1, temp);
+				}
+			}
+		}
+	}
 }
